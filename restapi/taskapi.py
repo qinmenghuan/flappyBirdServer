8e3930f6e6ca788f1710e08b896355edc0189dfb
qinmenghuan <1250397216@qq.com>
2018-09-27 09:43:30 +0800

    delete task

--- restapi/taskapi.py
+++ restapi/taskapi.py
@@ -29,3 +29,16 @@ def get_task():
     sqlstr = "SELECT t.task_id,t.taskname,t.starttime,t.endtime FROM xfz_task AS t WHERE t.isdeleted=0 AND t.weekindex LIKE '%%"+weekindex+"%%'"
     listResult = sqlutils.exec_sql(sqlstr)
     return json.dumps({'tasklist': listResult}, cls=common.CJsonEncoder.CJsonEncoder)
+
+# 根据任务id删除任务
+@taskapi.route('/deleteTask',methods=['GET'])
+def delete_task():
+    # 获取任务id
+    taskid = request.args.get('taskid')
+    if taskid is None:
+        taskid =""
+    print(taskid)
+    # 拼接执行sql
+    sqlstr = "UPDATE xfz_task SET isdeleted=1 WHERE task_id = "+str(taskid)
+    count = sqlutils.exec_txsql(sqlstr)
+    return str(count)
