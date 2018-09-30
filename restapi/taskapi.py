2a688b115bdae99d2c6afa7a915d8eb9c3333bd0
qinmenghuan <1250397216@qq.com>
2018-09-30 10:12:55 +0800

    task api

--- restapi/taskapi.py
+++ restapi/taskapi.py
@@ -11,6 +11,7 @@ import json
 import common.CJsonEncoder
 import common.mysqldemo
 from restapi.utils import json_response, JSON_MIME_TYPE,RESPONSE_CODE
+import time
 
 taskapi = Blueprint(r'taskapi', __name__)
 # 初始化sql操作类
@@ -26,7 +27,7 @@ def get_task():
         weekindex=""
     print(weekindex)
     # 拼接执行sql
-    sqlstr = "SELECT t.task_id,t.taskname,t.starttime,t.endtime FROM xfz_task AS t WHERE t.isdeleted=0 AND t.weekindex LIKE '%%"+weekindex+"%%'"
+    sqlstr = "SELECT t.taskid,t.taskname,t.starttime,t.endtime FROM xfz_taskrule AS t WHERE t.isdeleted=0 AND t.weekindex LIKE '%%"+weekindex+"%%'"
     listResult = sqlutils.exec_sql(sqlstr)
     return json.dumps({'tasklist': listResult}, cls=common.CJsonEncoder.CJsonEncoder)
 
@@ -42,3 +43,24 @@ def delete_task():
     sqlstr = "UPDATE xfz_task SET isdeleted=1 WHERE task_id = "+str(taskid)
     count = sqlutils.exec_txsql(sqlstr)
     return str(count)
+
+# 根据日期或者用户id任务列表
+@taskapi.route('/getTaskList',methods=['GET'])
+def get_tasklist():
+    # 获取参数
+    userid = request.args.get('userid')
+    if userid is None:
+        userid=""
+    searchDate= request.args.get('searchDate')
+    if searchDate is None:
+        searchDate=time.strftime('%Y-%m-%d',time.localtime())
+
+    # 拼接执行sql
+    sqlstr = "SELECT t.id,t.taskid,s.taskname,s.starttime,s.endtime FROM xfz_dailytask AS t " \
+             "LEFT JOIN xfz_taskrule AS s ON t.taskid=s.taskid " \
+             "WHERE t.userid={0} AND DATE_FORMAT(t.create_time,'%Y-%m-%d')='{1}'".format(userid,searchDate)
+
+    print(sqlstr)
+    listResult = sqlutils.exec_sql(sqlstr)
+
+    return json.dumps({'tasklist': listResult}, cls=common.CJsonEncoder.CJsonEncoder)
